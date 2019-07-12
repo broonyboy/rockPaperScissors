21107386adc481080ebef0ee3a0a5172d1b72233
Neale Brown <neale.d.brown@gmail.com>
2019-07-12 17:37:12 +0100

    Random Rock Paper Scissors for Computer

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // Played in the console
+let computerSelection = "";
 
 function computerPlay() {
 randomNumber = Math.floor(Math.random() * 3) + 1;
-
+if (randomNumber == 1) {
+    computerSelection = "Rock";
+} else if (randomNumber == 2) {
+    computerSelection = "Paper";
+} else {
+    computerSelection = "Scissors";
+}
+return computerSelection;
 }
 
 computerPlay();
